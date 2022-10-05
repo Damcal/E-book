29b8b59c34067aa8bb37f1c00a716af42a586ec9
Ajayi Caleb Oluwapelumi <Oluwapelumiajayico@gmail.com>
2022-10-05 12:34:54 +0100

    e-book

---
+++ www/js/index.js
@@ -0,0 +1,9 @@
+const numb = document.querySelector(".number");
+    let counter = 0;
+    window.onload = () =>{
+    setTimeout(() => {
+    window.location.href='homepage.html';
+    }, 2000);
+}
+
+
